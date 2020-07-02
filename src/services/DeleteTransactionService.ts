e0992c54f3710049e45bd4c457b87a2de029a4d6
Jin <jinoliveira74@gmail.com>
2020-07-02 10:01:12 -0300

    Should be able delete complete

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const checkTransaction = await transactionRepository.findOne(id);
+
+    if (!checkTransaction) {
+      throw new AppError('Transaction id invalid');
+    }
+
+    await transactionRepository.delete(id);
   }
 }
 
